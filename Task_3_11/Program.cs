c044d3418491e81c4daf68c3476d938f44cfe0a4
katri <kolesnikova.k94@gmail.com>
2025-03-12 23:07:07 +0400

    Домашняя работа 3 тема

---
+++ Task_3_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Task_3_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ширину участка");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину участка");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину 1го дома");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину 1го дома");
+            int q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину 2го дома");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину 2го дома");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            if ((p + r <= a && Math.Max(q, s) <= b) ||
+                (q + s <= b && Math.Max(p, r) <= a) ||
+                (p + s <= a && Math.Max(q, r) <= b) ||
+                (p + s <= b && Math.Max(q, r) <= a))
+            {
+                Console.WriteLine("Можно разместить дома с указанными габаритами на этом участке");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось разместить дома с указанными габаритами на этом участке");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_3_7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int first = Convert.ToInt32(Console.ReadLine());
+            string result = (9 < first && first < 100) ? "Да" : "Нет";
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
