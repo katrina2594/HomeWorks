314358072fce6d77e16bad3b74e9713490ad1820
katri <kolesnikova.k94@gmail.com>
2025-03-22 00:09:00 +0400

    task_4

---
+++ Task_4_4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число a > 0");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Введите целое число b < 0");
+            string input2 = Console.ReadLine();
+            int a = 0;
+            int b = 0;
+            if (int.TryParse(input1, out a) && a > 0 && int.TryParse(input2, out b) && b < 0)
+            {
+                int result1 = 1;
+                for (int i = 1; i <=-b; i++)
+                {
+                    result1 *= a;
+                }
+
+                double result2 = 1.0 / result1;
+                Console.WriteLine(result2);
+
+            }
+            else
+            {
+                Console.WriteLine("Введите корректные числа");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
---
+++ Task_4_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string input = "";
+            do
+            {
+                Console.WriteLine("Введите число в диапазоне [20;60]");
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out number) || number < 20 || number > 60);
+            Console.ReadKey();
+        }
+    }
+}
