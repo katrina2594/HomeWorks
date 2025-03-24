78eb277f2f58885a2ce7867b3c4bafa80f386040
katri <kolesnikova.k94@gmail.com>
2025-03-24 22:09:27 +0400

    Работа над ошибками

--- Task_3_7/Program.cs
+++ Task_3_7/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Введите число");
             int first = Convert.ToInt32(Console.ReadLine());
+            first = Math.Abs(first);
             string result = (9 < first && first < 100) ? "Да" : "Нет";
             Console.WriteLine(result);
             Console.ReadKey();
