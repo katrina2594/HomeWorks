6d54fd44c82ac6557f500f7f9b3e03dadd05072a
katri <kolesnikova.k94@gmail.com>
2025-04-08 22:21:09 +0400

    Task_9

---
+++ Task_9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Война и мир", "Л. Толстой", 1869, 1225);
+            Book book2 = new Book("Преступление и наказание", "Ф. Достоевский", 1866, 300);
+            Console.WriteLine(book1.GetInfo());
+            Console.WriteLine(book2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_9_2/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9_2
+{
+    class BankAccount
+    {
+        private decimal _balance;
+        public static int TotalAccounts { get; private set; }
+        readonly int AccountNumber;
+        public decimal Balance { get; private set; }
+        public BankAccount()
+        {
+            TotalAccounts++;
+            Random rnd = new Random();
+            AccountNumber = rnd.Next(1000, 9999);
+            Console.WriteLine($"Создан новый счет #{AccountNumber}," +
+                    $"\nтекущий баланс: {Balance} руб.");
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"На счет #{AccountNumber} поступило {amount} руб." +
+                    $"\nтекущий баланс: {Balance} руб.");
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Со счета #{AccountNumber} успешно списано {amount} руб." +
+                    $"\nтекущий баланс: {Balance} руб.");
+            }
+            else
+            {
+                throw new ArgumentException($"Суммы на счете #{AccountNumber} недостаточно для списания {amount} руб." +
+                    $"\nтекущий баланс: {Balance} руб.");
+            }
+        }
+    }
+}
