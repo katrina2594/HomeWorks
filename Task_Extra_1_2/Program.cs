4087db3c46ba5e5f35649b48ac231afe24b63c0a
katri <kolesnikova.k94@gmail.com>
2025-06-21 20:54:43 +0500

    Task_Extra

---
+++ Task_Extra_1_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task_Extra_1_2
+{
+    internal class Program
+    {
+        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст для шифрования");
+            string text = Console.ReadLine().ToUpper();
+            string newWord = Encrypt(text);
+            Console.WriteLine($"Зашифрованный текст: {newWord}");
+            string newWord2 = Encrypt(newWord);
+            Console.WriteLine($"Дешифрованный текст: {newWord2}");
+            Console.ReadKey();
+        }
+        static string Encrypt(string text)
+        {
+            string newWord = "";
+            foreach (char letter in text)
+            {
+                int indexLetter = alphabet.IndexOf(letter);
+                if (indexLetter >= 0)
+                {
+                    int indexAfter = (alphabet.Length - 1) - indexLetter;
+                    newWord = newWord + alphabet[indexAfter];
+                }
+                else
+                {
+                    newWord = newWord + letter;
+                }
+            }
+            return newWord;
+        }
+
+    }
+}
---
+++ Task_Extra_3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_Extra_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1 = new Game();
+            game1.Start();
+            Console.ReadKey();
+        }
+    }
+}
