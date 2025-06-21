1a69bf50bc49ddf6f3e9fbe5abfdbbcf3db9ba2d
katri <kolesnikova.k94@gmail.com>
2025-06-21 23:56:18 +0500

    Task_14

---
+++ Task_14_1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_14_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { -2, -1, 0, 1, 2, 3 };
+            int[] m1 = Transform(numbers, value => value * 2);
+            int[] m2 = Transform(numbers, value => value * value);
+            int[] m3 = Transform(numbers, value => Math.Abs(value));
+
+            Console.WriteLine($"Удвоенный массив: {string.Join(",", m1)}");
+            Console.WriteLine($"Массив в квадрате: {string.Join(",", m2)}");
+            Console.WriteLine($"Модули чисел исходного массива: {string.Join(",", m3)}");
+            Console.ReadKey();
+
+        }
+        public delegate int Transformer(int value);
+        public static int[] Transform(int[] numbers, Transformer transformer)
+        {
+            int[] newNumbers = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newNumbers[i] = transformer(numbers[i]);
+            }
+            return newNumbers;
+        }
+    }
+}
+
---
+++ Task_14_2/SmartHomeSystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_2
+{
+    public delegate void Logger(string device, string state);
+    public class SmartHomeSystem
+    {
+        public event Logger Logger;
+        bool lightStatus = false;
+        int term = 0;
+        bool doorStatus = false;
+        public void TurnOnLight()
+        {
+            lightStatus = true;
+            Logger?.Invoke("Освещение", "включено");
+        }
+        public void TurnOffLight()
+        {
+            lightStatus = false;
+            Logger?.Invoke("Освещение", "выключено");
+        }
+        public void SetTemperature(int newTemp)
+        {
+            term = newTemp;
+            Logger?.Invoke("Термостат", $"Температура изменена на {newTemp}°C");
+        }
+        public void LockDoor()
+        {
+            doorStatus = true;
+            Logger?.Invoke("Дверь", "закрыта");
+        }
+        public void UnlockDoor()
+        {
+            doorStatus = false;
+            Logger?.Invoke("Дверь", "открыта");
+        }
+    }
+    
+}
