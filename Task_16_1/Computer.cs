7329d779f26dc8041f4ad5ff679cd2b23905b17d
katri <kolesnikova.k94@gmail.com>
2025-06-28 22:50:57 +0500

    Task_16

---
+++ Task_16_1/Computer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_16_1
+{
+    class Computer
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string ProcessorType { get; set; }
+        public int Frequency { get; set; }
+        public int RAMsize { get; set; }
+        public int HardDriveSize { get; set; }
+        public int VideocardSize { get; set; }
+        public decimal Cost { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return $"Код: {Code} Модель: {Name} Процессор: {ProcessorType} Частота процессора: {Frequency} Объем RAM: {RAMsize} Объем диска: {HardDriveSize} Видеокарта: {VideocardSize} Цена: {Cost} В наличии: {Count}шт.";
+        }
+
+    }
+}
